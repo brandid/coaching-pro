63bce338e6bf79a88822aa96a934ebebb8c5accc
Ronald Huereca <ronalfy@gmail.com>
2022-04-28 09:46:00 -0500

    Updating block patterns with colors.

--- lib/block-patterns.php
+++ lib/block-patterns.php
@@ -38,7 +38,27 @@ function coachingpro_register_block_patterns() {
 
 		// Main Hero.
 		/* ------------------------------------------------------------------ */
-		$mainhero_pattern_content = "<!-- wp:genesis-blocks/gb-columns {\"backgroundImgURL\":\"$sample_hero_image\",\"columns\":1,\"layout\":\"one-column\",\"align\":\"full\",\"marginBottom\":60,\"paddingTop\":200,\"paddingRight\":30,\"paddingBottom\":200,\"paddingLeft\":30,\"textColor\":\"white\",\"backgroundColor\":\"colorone\",\"columnMaxWidth\":600} -->\n<div class=\"wp-block-genesis-blocks-gb-columns gb-layout-columns-1 one-column gb-background-cover gb-background-no-repeat has-colorone-background-color has-white-color gb-columns-center alignfull\" style=\"margin-bottom:60px;padding-top:200px;padding-right:30px;padding-bottom:200px;padding-left:30px;background-image:url($sample_hero_image)\"><div class=\"gb-layout-column-wrap gb-block-layout-column-gap-2 gb-is-responsive-column\" style=\"max-width:600px\"><!-- wp:genesis-blocks/gb-column {\"textAlign\":\"center\"} -->\n<div class=\"wp-block-genesis-blocks-gb-column gb-block-layout-column\"><div class=\"gb-block-layout-column-inner\" style=\"text-align:center\"><!-- wp:heading {\"level\":1} -->\n<h1><strong>Coaching just got better.</strong></h1>\n<!-- /wp:heading -->\n\n<!-- wp:paragraph -->\n<p>Whatever your expertise, whatever your calling, Coaching Pro makes it easy to put yourself out there.</p>\n<!-- /wp:paragraph -->\n\n<!-- wp:buttons {\"contentJustification\":\"center\"} -->\n<div class=\"wp-block-buttons is-content-justification-center\"><!-- wp:button {\"backgroundColor\":\"white\",\"textColor\":\"colortwo\"} -->\n<div class=\"wp-block-button\"><a class=\"wp-block-button__link has-colortwo-color has-white-background-color has-text-color has-background\" href=\"#\">Buy Now</a></div>\n<!-- /wp:button --></div>\n<!-- /wp:buttons --></div></div>\n<!-- /wp:genesis-blocks/gb-column --></div></div>\n<!-- /wp:genesis-blocks/gb-columns -->";
+		ob_start();
+		?>
+		<!-- wp:genesis-blocks/gb-columns {"backgroundImgURL":"https://coachingpro.local/wp-content/themes/coaching-pro/images/sample-hero.jpg","columns":1,"layout":"one-column","align":"full","marginBottom":60,"paddingTop":200,"paddingRight":30,"paddingBottom":200,"paddingLeft":30,"textColor":"white","backgroundColor":"colorone","columnMaxWidth":600} -->
+<div class="wp-block-genesis-blocks-gb-columns gb-layout-columns-1 one-column gb-background-cover gb-background-no-repeat has-colorone-background-color has-white-color gb-columns-center alignfull" style="margin-bottom:60px;padding-top:200px;padding-right:30px;padding-bottom:200px;padding-left:30px;background-image:url(<?php echo esc_url( $sample_hero_image ); ?>)"><div class="gb-layout-column-wrap gb-block-layout-column-gap-2 gb-is-responsive-column" style="max-width:600px"><!-- wp:genesis-blocks/gb-column {"textAlign":"center"} -->
+<div class="wp-block-genesis-blocks-gb-column gb-block-layout-column"><div class="gb-block-layout-column-inner" style="text-align:center"><!-- wp:heading {"level":1,"textColor":"white"} -->
+<h1 class="has-white-color has-text-color"><strong>Coaching just got better.</strong></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Whatever your expertise, whatever your calling, Coaching Pro makes it easy to put yourself out there.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"white","textColor":"theme-color-4"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-theme-color-4-color has-white-background-color has-text-color has-background" href="#">Buy Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:genesis-blocks/gb-column --></div></div>
+<!-- /wp:genesis-blocks/gb-columns -->
+<?php
+		$mainhero_pattern_content = ob_get_clean();
 
 		register_block_pattern(
 			'coachingpro/mainhero-pattern',
@@ -55,7 +75,33 @@ function coachingpro_register_block_patterns() {
 
 		// Hero 2-columns.
 		/* ------------------------------------------------------------------ */
-		$hero2columns_pattern_content = "<!-- wp:genesis-blocks/gb-columns {\"columns\":2,\"layout\":\"gb-2-col-equal\",\"columnsGap\":0,\"align\":\"full\",\"backgroundColor\":\"bgcolorone\"} -->\n<div class=\"wp-block-genesis-blocks-gb-columns gb-layout-columns-2 gb-2-col-equal has-bgcolorone-background-color alignfull\"><div class=\"gb-layout-column-wrap gb-block-layout-column-gap-0 gb-is-responsive-column\"><!-- wp:genesis-blocks/gb-column {\"textAlign\":\"center\",\"columnVerticalAlignment\":\"center\"} -->\n<div class=\"wp-block-genesis-blocks-gb-column gb-block-layout-column gb-is-vertically-aligned-center\"><div class=\"gb-block-layout-column-inner\" style=\"text-align:center\"><!-- wp:image {\"id\":9999,\"sizeSlug\":\"full\",\"linkDestination\":\"none\"} -->\n<figure class=\"wp-block-image size-full\"><img src=\"$sample_hero_image\" alt=\"\" class=\"wp-image-9999\"/></figure>\n<!-- /wp:image --></div></div>\n<!-- /wp:genesis-blocks/gb-column -->\n\n<!-- wp:genesis-blocks/gb-column {\"textAlign\":\"center\",\"paddingSync\":true,\"padding\":40,\"columnVerticalAlignment\":\"center\"} -->\n<div class=\"wp-block-genesis-blocks-gb-column gb-block-layout-column gb-is-vertically-aligned-center\"><div class=\"gb-block-layout-column-inner\" style=\"padding:40px;text-align:center\"><!-- wp:heading {\"level\":1} -->\n<h1><strong>Coaching just got better.</strong></h1>\n<!-- /wp:heading -->\n\n<!-- wp:paragraph -->\n<p>Whatever your expertise, whatever your calling, Coaching Pro makes it easy to put yourself out there.</p>\n<!-- /wp:paragraph -->\n\n<!-- wp:buttons {\"contentJustification\":\"center\"} -->\n<div class=\"wp-block-buttons is-content-justification-center\"><!-- wp:button {\"backgroundColor\":\"colortwo\",\"textColor\":\"white\"} -->\n<div class=\"wp-block-button\"><a class=\"wp-block-button__link has-white-color has-colortwo-background-color has-text-color has-background\" href=\"#\">Buy Now</a></div>\n<!-- /wp:button --></div>\n<!-- /wp:buttons --></div></div>\n<!-- /wp:genesis-blocks/gb-column --></div></div>\n<!-- /wp:genesis-blocks/gb-columns -->";
+		ob_start();
+		?>
+		<!-- wp:genesis-blocks/gb-columns {"columns":2,"layout":"gb-2-col-equal","columnsGap":0,"align":"full","backgroundColor":"bgcolorone"} -->
+<div class="wp-block-genesis-blocks-gb-columns gb-layout-columns-2 gb-2-col-equal has-bgcolorone-background-color alignfull"><div class="gb-layout-column-wrap gb-block-layout-column-gap-0 gb-is-responsive-column"><!-- wp:genesis-blocks/gb-column {"textAlign":"center","columnVerticalAlignment":"center"} -->
+<div class="wp-block-genesis-blocks-gb-column gb-block-layout-column gb-is-vertically-aligned-center"><div class="gb-block-layout-column-inner" style="text-align:center"><!-- wp:image {"id":9999,"sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="<?php echo esc_url( $sample_hero_image ); ?>" alt="" class="wp-image-9999"/></figure>
+<!-- /wp:image --></div></div>
+<!-- /wp:genesis-blocks/gb-column -->
+
+<!-- wp:genesis-blocks/gb-column {"textAlign":"center","paddingSync":true,"padding":40,"columnVerticalAlignment":"center"} -->
+<div class="wp-block-genesis-blocks-gb-column gb-block-layout-column gb-is-vertically-aligned-center"><div class="gb-block-layout-column-inner" style="padding:40px;text-align:center"><!-- wp:heading {"level":1,"textColor":"black"} -->
+<h1 class="has-black-color has-text-color"><strong>Coaching just got better.</strong></h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Whatever your expertise, whatever your calling, Coaching Pro makes it easy to put yourself out there.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"theme-color-4","textColor":"white"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-white-color has-theme-color-4-background-color has-text-color has-background" href="#">Buy Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:genesis-blocks/gb-column --></div></div>
+<!-- /wp:genesis-blocks/gb-columns -->
+		<?php
+		$hero2columns_pattern_content = ob_get_clean();
 
 		register_block_pattern(
 			'coachingpro/herotwocol-pattern',
@@ -72,7 +118,43 @@ function coachingpro_register_block_patterns() {
 
 		// Basic 2-columns.
 		/* ------------------------------------------------------------------ */
-		$basic2columns_pattern_content = "<!-- wp:genesis-blocks/gb-columns {\"columns\":2,\"layout\":\"gb-2-col-equal\",\"columnsGap\":0,\"align\":\"full\"} -->\n<div class=\"wp-block-genesis-blocks-gb-columns gb-layout-columns-2 gb-2-col-equal alignfull\"><div class=\"gb-layout-column-wrap gb-block-layout-column-gap-0 gb-is-responsive-column\"><!-- wp:genesis-blocks/gb-column {\"backgroundColor\":\"bgcolorone\",\"textAlign\":\"center\",\"paddingSync\":true,\"padding\":40,\"columnVerticalAlignment\":\"center\"} -->\n<div class=\"wp-block-genesis-blocks-gb-column gb-block-layout-column gb-is-vertically-aligned-center\"><div class=\"gb-block-layout-column-inner has-bgcolorone-background-color\" style=\"padding:40px;text-align:center\"><!-- wp:heading -->\n<h2><strong>Coaching just got better.</strong></h2>\n<!-- /wp:heading -->\n\n<!-- wp:paragraph -->\n<p>Whatever your expertise, whatever your calling, Coaching Pro makes it easy to put yourself out there.</p>\n<!-- /wp:paragraph -->\n\n<!-- wp:buttons {\"contentJustification\":\"center\"} -->\n<div class=\"wp-block-buttons is-content-justification-center\"><!-- wp:button {\"backgroundColor\":\"colortwo\",\"textColor\":\"white\"} -->\n<div class=\"wp-block-button\"><a class=\"wp-block-button__link has-white-color has-colortwo-background-color has-text-color has-background\" href=\"#\">Buy Now</a></div>\n<!-- /wp:button --></div>\n<!-- /wp:buttons --></div></div>\n<!-- /wp:genesis-blocks/gb-column -->\n\n<!-- wp:genesis-blocks/gb-column {\"backgroundColor\":\"bgcolortwo\",\"textAlign\":\"center\",\"paddingSync\":true,\"padding\":40,\"columnVerticalAlignment\":\"center\"} -->\n<div class=\"wp-block-genesis-blocks-gb-column gb-block-layout-column gb-is-vertically-aligned-center\"><div class=\"gb-block-layout-column-inner has-bgcolortwo-background-color\" style=\"padding:40px;text-align:center\"><!-- wp:heading -->\n<h2><strong>Coaching just got better.</strong></h2>\n<!-- /wp:heading -->\n\n<!-- wp:paragraph -->\n<p>Whatever your expertise, whatever your calling, Coaching Pro makes it easy to put yourself out there.</p>\n<!-- /wp:paragraph -->\n\n<!-- wp:buttons {\"contentJustification\":\"center\"} -->\n<div class=\"wp-block-buttons is-content-justification-center\"><!-- wp:button {\"backgroundColor\":\"colortwo\",\"textColor\":\"white\"} -->\n<div class=\"wp-block-button\"><a class=\"wp-block-button__link has-white-color has-colortwo-background-color has-text-color has-background\" href=\"#\">Buy Now</a></div>\n<!-- /wp:button --></div>\n<!-- /wp:buttons --></div></div>\n<!-- /wp:genesis-blocks/gb-column --></div></div>\n<!-- /wp:genesis-blocks/gb-columns -->";
+		ob_start();
+		?>
+		<!-- wp:genesis-blocks/gb-columns {"columns":2,"layout":"gb-2-col-equal","columnsGap":0,"align":"full"} -->
+<div class="wp-block-genesis-blocks-gb-columns gb-layout-columns-2 gb-2-col-equal alignfull"><div class="gb-layout-column-wrap gb-block-layout-column-gap-0 gb-is-responsive-column"><!-- wp:genesis-blocks/gb-column {"backgroundColor":"bgcolorone","textAlign":"center","paddingSync":true,"padding":40,"columnVerticalAlignment":"center"} -->
+<div class="wp-block-genesis-blocks-gb-column gb-block-layout-column gb-is-vertically-aligned-center"><div class="gb-block-layout-column-inner has-bgcolorone-background-color" style="padding:40px;text-align:center"><!-- wp:heading -->
+<h2><strong>Coaching just got better.</strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Whatever your expertise, whatever your calling, Coaching Pro makes it easy to put yourself out there.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"theme-color-4","textColor":"white"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-white-color has-theme-color-4-background-color has-text-color has-background" href="#">Buy Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:genesis-blocks/gb-column -->
+
+<!-- wp:genesis-blocks/gb-column {"backgroundColor":"bgcolortwo","textAlign":"center","paddingSync":true,"padding":40,"columnVerticalAlignment":"center"} -->
+<div class="wp-block-genesis-blocks-gb-column gb-block-layout-column gb-is-vertically-aligned-center"><div class="gb-block-layout-column-inner has-bgcolortwo-background-color" style="padding:40px;text-align:center"><!-- wp:heading -->
+<h2><strong>Coaching just got better.</strong></h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Whatever your expertise, whatever your calling, Coaching Pro makes it easy to put yourself out there.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center","orientation":"horizontal"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"theme-color-4","textColor":"white"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-white-color has-theme-color-4-background-color has-text-color has-background" href="#">Buy Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:genesis-blocks/gb-column --></div></div>
+<!-- /wp:genesis-blocks/gb-columns -->
+		<?php
+		$basic2columns_pattern_content = ob_get_clean();
 
 		register_block_pattern(
 			'coachingpro/basictwocol-pattern',
